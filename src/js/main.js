f9ce0b9b58ab04d81558387b65a28b8a90ac6618
Audrey Carlsen <acarlsen@seattletimes.com>
2016-10-24 14:41:37 -0700

    adds counter

--- src/js/main.js
+++ src/js/main.js
@@ -5,15 +5,23 @@
 require("component-responsive-frame/child");
 var savage = require("savage-query");
 var $ = require("./lib/qsa.js");
+var closest = require("./lib/closest");
+
+var count = 50;
 
 $(".st0").forEach(function(state) {
   state.addEventListener("click", function() {
-    if (state.getAttribute("class").includes("red")) {
+    if (!state.getAttribute("data-name")) {
+      state = state.closest("g");
+    }
+    if (state.getAttribute("class") && state.getAttribute("class").includes("red")) {
       savage(state).removeClass("red");
       savage(state).addClass("blue");
     } else {
       savage(state).removeClass("blue");
       savage(state).addClass("red");
     }
+    count = 50 - $(".red").length + $(".blue").length;
+    document.querySelector(".count").innerHTML = count;
   })
 })
